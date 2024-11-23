846fc6e13ebb1895d79b23b9e71afd8ac1824fe5
Mauro Sakugawa <maurotoshiuki@gmail.com>
2024-11-23 16:37:22 -0300

    Adaptação do arquivo urls.py às refatorações da view e às restrições de níveis de acesso

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from accounts.views import dashboard_view  # Import correto da view desejada
 from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('dashboard/', dashboard_view, name='dashboard'),  # Use a view correta
 ]
